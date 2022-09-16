4a347195f147165ccbd30e7aad882b18cea14c65
sergio1204 <sheriff1204@gmail.com>
2022-09-17 01:36:32 +0300

    add conf

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+killall -q slstatus; slstatus &
+killall -q picom; picom &
+setxkbmap -layout us,ru -option grp:alt_shift_toggle &
+feh --bg-scale /home/sergey/Pictures/kiberpank_gorod.jpg &
