1bcaf07698370901b232a87c10cbadfb6726d66f
sergio1204 <sheriff1204@gmail.com>
2022-10-22 19:52:43 +0300

    add patch

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -3,10 +3,15 @@
 #include <X11/XF86keysym.h>
 
 /* appearance -------------------------------------------------------------*/
-static const unsigned int borderpx = 3;      /* border pixel of windows */
-static const unsigned int snap     = 32;    /* snap pixel */
-static const int showbar           = 1;    /* 0 means no bar */
-static const int topbar            = 1;   /* 0 means bottom bar */
+static const unsigned int borderpx       = 3;   /* border pixel of windows */
+static const unsigned int snap           = 32;  /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor */
+static const int showsystray             = 1;   /* 0 means no systray */
+static const int showbar                 = 1;   /* 0 means no bar */
+static const int topbar                  = 1;   /* 0 means bottom bar */
 static const char *fonts[]         = { "JetBrains Mono:size=10" };
 static const char colour1[]        = "#23252e";
 static const char colour2[]        = "#81C1FF";
@@ -130,8 +135,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_a,      focusstack,       {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_s,      incnmaster,       {.i = +1 } },
 	{ MODKEY,                       XK_s,      incnmaster,       {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_Left,   setmfact,         {.f = -0.05} },
-	{ MODKEY|ControlMask,           XK_Right,  setmfact,         {.f = +0.05} },
+	{ MODKEY|ControlMask,           XK_Left,   setmfact,         {.f = +0.05} },
+	{ MODKEY|ControlMask,           XK_Right,  setmfact,         {.f = -0.05} },
 	{ MODKEY,                       XK_g,      zoom,             {0} },
 	{ MODKEY|ShiftMask,             XK_Tab,    view,             {0} },
 	{ MODKEY,                       XK_q,      killclient,       {0} },
@@ -164,8 +169,8 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
