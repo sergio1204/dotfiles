94507bc6e53fc5622bf9934e53e83438966a7d4a
sergio1204 <sheriff1204@gmail.com>
2022-11-05 23:18:03 +0300

    upd conf

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -3,4 +3,4 @@
 killall -q slstatus; slstatus &
 killall -q picom; picom &
 setxkbmap -layout us,ru -option grp:alt_shift_toggle &
-feh --bg-scale /home/sergey/Pictures/kiberpank_gorod.jpg &
+feh --bg-scale /home/sergey/Pictures/metro-eskalator.jpg &
