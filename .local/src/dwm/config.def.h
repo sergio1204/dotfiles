9241e69032a9f7578352fdc3ffe688e26b3ad865
sergio1204 <sheriff1204@gmail.com>
2023-08-07 13:29:40 +0300

    upd conf

--- .local/src/dwm/config.def.h
+++ .local/src/dwm/config.def.h
@@ -176,18 +176,20 @@ static const Key keys[]             = {
 /* button definitions ==========================================================================*/
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-    /* click                event mask      button          function        argument */
-    { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-    { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-    { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-    { ClkStatusText,        0,              Button4,        spawn,          {.v = volupcmd } },
-    { ClkStatusText,        0,              Button5,        spawn,          {.v = voldowncmd } },
-    { ClkStatusText,        0,              Button2,        spawn,          {.v = volmutecmd } },
-    { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-    { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-    { ClkTagBar,            0,              Button1,        view,           {0} },
-    { ClkTagBar,            0,              Button3,        toggleview,     {0} },
-    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    /* click                event mask      button          function          argument */
+    { ClkLtSymbol,          0,              Button1,        setlayout,        {0} },
+    { ClkLtSymbol,          0,              Button3,        setlayout,        {.v = &layouts[2]} },
+    { ClkWinTitle,          0,              Button2,        zoom,             {0} },
+    { ClkStatusText,        0,              Button4,        spawn,            {.v = volupcmd } },
+    { ClkStatusText,        0,              Button5,        spawn,            {.v = voldowncmd } },
+    { ClkStatusText,        0,              Button2,        spawn,            {.v = volmutecmd } },
+    { ClkClientWin,         MODKEY,         Button1,        movemouse,        {0} },
+    { ClkClientWin,         MODKEY,         Button2,        togglefloating,   {0} },
+    { ClkClientWin,         MODKEY,         Button3,        resizemouse,      {0} },
+    { ClkTagBar,            0,              Button1,        view,             {0} },
+    { ClkTagBar,            0,              Button3,        toggleview,       {0} },
+    { ClkTagBar,            MODKEY,         Button1,        tag,              {0} },
+    { ClkTagBar,            MODKEY,         Button3,        toggletag,        {0} },
+    { ClkTagBar,            0,              Button4,        shiftviewclients, { .i = +1 } },
+    { ClkTagBar,            0,              Button5,        shiftviewclients, { .i = -1 } },
 };
