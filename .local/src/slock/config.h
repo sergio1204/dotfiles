0548f82f47a067f794eeefd2fd2f31062ce40d79
sergio1204 <sheriff1204@gmail.com>
2023-08-24 22:19:48 +0300

    add conf

--- .local/src/slock/config.h
+++ .local/src/slock/config.h
@@ -3,29 +3,10 @@ static const char *user  = "sergey";
 static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
-    [BACKGROUND] = "black",      /* after initialization */
-    [INIT]       = "#282C34",   /* after initialization */
-    [INPUT]      = "#325081",  /* during input */
-    [FAILED]     = "#9D3546", /* wrong password */
+    [INIT] =   "#23252e",   /* after initialization */
+    [INPUT] =  "#2b4051",  /* during input */
+    [FAILED] = "#763030", /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
-
-/* insert grid pattern with scale 1:1, the size can be changed with logosize */
-static const int logosize = 75;
-static const int logow    = 12; /* grid width and height for right center alignment*/
-static const int logoh    = 6;
-
-static XRectangle rectangles[9] = {
-    /* x    y     w     h */
-    { 0,    3,    1,    3 },
-    { 1,    3,    2,    1 },
-    { 0,    5,    8,    1 },
-    { 3,    0,    1,    5 },
-    { 5,    3,    1,    2 },
-    { 7,    3,    1,    2 },
-    { 8,    3,    4,    1 },
-    { 9,    4,    1,    2 },
-    { 11,   4,    1,    2 },
-};
