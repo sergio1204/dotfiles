b50c2fe44f2ead95f87da60decedcfa743c5c25e
sergio1204 <sheriff1204@gmail.com>
2025-06-15 19:45:33 +0300

    rm conf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -694,7 +694,7 @@ require('lazy').setup {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, lua = true }
+        local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
